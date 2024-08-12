17a85e56d8611b6371b9c68402690ace23f2be97
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 14:16:05 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Create an empty array called 'todoList' to store your to-do list items.
+
+
+// You will be implementing a loop that repeatedly displays possible options, takes user input, and performs actions accordingly. 
+// Print "Welcome to the To-Do List App"
+
+
+// Print the options 
+// 1 - display the list
+// 2 - add new task to end
+// 3 - add new task to beginning
+// 4 - delete first task
+// 5 - exit
+
+
+// Define a variable called userChoice and set it to 0
+
+
+// Create a while loop that runs while the user doesn't type in 5
+
+
+// In the loop, use conditionals to match the action to the option 
+// 1 - display the list
+// 2 - add new task to end
+// 3 - add new task to beginning
+// 4 - delete first task
+// 5 - exit
+
+
+
+
+
+
+
+
+
